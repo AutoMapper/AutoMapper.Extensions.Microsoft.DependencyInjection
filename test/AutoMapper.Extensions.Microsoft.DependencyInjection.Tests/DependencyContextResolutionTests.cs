46977f5ae962f1f3458078d5840d9429c0c1915b
Christian Prochnow <christian.prochnow@smarthouse.de>
2017-07-04 07:55:14 +0200

    Added test for type resolution via DependencyContext.

---
+++ test/AutoMapper.Extensions.Microsoft.DependencyInjection.Tests/DependencyContextResolutionTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyModel;
+
+namespace AutoMapper.Extensions.Microsoft.DependencyInjection.Tests
+{
+    using System;
+    using global::Microsoft.Extensions.DependencyInjection;
+    using Shouldly;
+    using Xunit;
+
+    public class DependencyContextResolutionTests
+    {
+        private readonly IServiceProvider _provider;
+
+        public DependencyContextResolutionTests()
+        {
+            var dependencyContext = DependencyContext.Load(typeof(DependencyContextResolutionTests).GetTypeInfo().Assembly);
+
+            IServiceCollection services = new ServiceCollection();
+            services.AddAutoMapper(mapper =>
+            {
+                mapper.AddTypeConverters(dependencyContext);
+                mapper.AddValueResolvers(dependencyContext);
+                mapper.AddProfiles(dependencyContext);
+            });
+
+            _provider = services.BuildServiceProvider();
+        }
+
+        [Fact]
+        public void ShouldResolveConfiguration()
+        {
+            _provider.GetService<IConfigurationProvider>().ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void ShouldConfigureProfiles()
+        {
+            _provider.GetService<IConfigurationProvider>().GetAllTypeMaps().Length.ShouldBe(2);
+        }
+
+        [Fact]
+        public void ShouldResolveMapper()
+        {
+            _provider.GetService<IMapper>().ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void ShouldResolveValueResolver()
+        {
+            _provider.GetServices<SomeValueResolver>().ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void ShouldResolveMemberValueResolver()
+        {
+            _provider.GetServices<SomeMemberValueResolver>().ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void ShouldResolveTypeConverter()
+        {
+            _provider.GetService<SomeTypeConverter>().ShouldNotBeNull();
+        }
+    }
+}
