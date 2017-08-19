9c6c7a4c96629872424442430844cc7508e295a6
Jimmy Bogard <jimmy.bogard@gmail.com>
2017-08-19 11:24:49 +1000

    Tests

---
+++ test/AutoMapper.Extensions.Microsoft.DependencyInjection.Tests/AppDomainResolutionTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AutoMapper.Extensions.Microsoft.DependencyInjection.Tests
+{
+    using System;
+    using System.Reflection;
+    using global::Microsoft.Extensions.DependencyModel;
+    using Shouldly;
+    using Xunit;
+
+    public class AppDomainResolutionTests
+    {
+        private readonly IServiceProvider _provider;
+
+        public AppDomainResolutionTests()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddAutoMapper();
+            _provider = services.BuildServiceProvider();
+        }
+
+        [Fact]
+        public void ShouldResolveConfiguration()
+        {
+            _provider.GetService<IConfigurationProvider>().ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void ShouldConfigureProfiles()
+        {
+            _provider.GetService<IConfigurationProvider>().GetAllTypeMaps().Length.ShouldBe(2);
+        }
+
+        [Fact]
+        public void ShouldResolveMapper()
+        {
+            _provider.GetService<IMapper>().ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void ShouldInitializeStatically()
+        {
+            _provider.GetService<IConfigurationProvider>().ShouldBeSameAs(Mapper.Configuration);
+        }
+    }
+}
