1d32bb0fb0581a4c89510e7248403e967719fa2a
wdspider <wdspider+github@gmail.com>
2018-01-26 09:05:02 -0800

    Added IApplicationBuilder.AssertAutoMapperConfigurationIsValid() extension method and test app to demo its use.

---
+++ src/AutoMapper.Extensions.Microsoft.DependencyInjection/ApplicationBuilderExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace AutoMapper
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Adds a <see cref="ConfigurationAssertMiddleware"/> that will call <see cref="IConfigurationProvider"/>.AssertConfigurationIsValid()
+        /// to verify that AutoMapper's configuration is valid.
+        /// </summary>
+        /// <remarks>
+        /// Since asserts are performed by exceptions, adding this middleware tends to return empty Internal Server Errors (500) unless
+        /// this middleware is added after <see cref="IApplicationBuilder"/>.UseDeveloperExceptionPage()
+        /// </remarks>
+        public static void AssertAutoMapperConfigurationIsValid(this IApplicationBuilder app) => app.UseMiddleware<ConfigurationAssertMiddleware>();
+    }
+
+    internal sealed class ConfigurationAssertMiddleware
+    {
+        #region Internal State
+        private readonly RequestDelegate nextStep;
+        private readonly IConfigurationProvider configurationProvider;
+        #endregion
+
+        public ConfigurationAssertMiddleware(RequestDelegate nextStep, IConfigurationProvider configurationProvider)
+        {
+            // Initialize internal state
+            this.nextStep = nextStep;
+            this.configurationProvider = configurationProvider;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            configurationProvider.AssertConfigurationIsValid();
+
+            return nextStep(httpContext);
+        }
+    }
+}
---
+++ src/TestWebApp/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+
+namespace TestWebApp
+{
+    internal sealed class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+    }
+
+    internal sealed class PersonDTO
+    {
+        public string LastName { get; set; }
+    }
+
+    internal sealed class PersonMappingProfile : Profile
+    {
+        public PersonMappingProfile()
+        {
+            // Maps intentionally mismatched to ensure config is invalid
+            CreateMap<Person, PersonDTO>();
+            CreateMap<PersonDTO, Person>();
+        }
+    }
+}
