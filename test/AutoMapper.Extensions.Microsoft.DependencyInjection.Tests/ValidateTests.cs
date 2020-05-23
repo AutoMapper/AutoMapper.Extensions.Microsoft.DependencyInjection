48d164ade2d54f8241e0a132d85bbb50f23943b5
GravlLift <jason.gravell@gmail.com>
2020-05-23 10:19:58 -0400

    Added validation test

---
+++ test/AutoMapper.Extensions.Microsoft.DependencyInjection.Tests/ValidateTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Xunit;
+
+namespace AutoMapper.Extensions.Microsoft.DependencyInjection.Tests
+{
+    public class ValidateTests
+    {
+        [Fact]
+        public void ShouldFailAssertForInvalidProfile()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+
+            // Act
+            serviceCollection.AddAutoMapper(true, typeof(InvalidProfile));
+
+            // Assert
+            var provider = serviceCollection.BuildServiceProvider();
+            Assert.Throws<AutoMapperConfigurationException>(() => provider.GetRequiredService<IMapper>());
+        }
+
+        class InvalidProfile : Profile
+        {
+            public InvalidProfile()
+            {
+                // PropertyTwo is unmapped, profile is invalid
+                CreateMap<Source, Destination>();
+            }
+        }
+
+        class Source
+        {
+            public string PropertyOne { get; set; }
+        }
+
+        class Destination
+        {
+            public string PropertyTwo { get; set; }
+        }
+    }
+}
